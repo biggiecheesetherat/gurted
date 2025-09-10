318a8a474e783211f2b38e192fccab17684b0676
Themeatly2 <143027785+biggiecheesetherat@users.noreply.github.com>
2025-09-10 06:57:33 -0700

    Update mod.rs

--- dns/src/config/mod.rs
+++ dns/src/config/mod.rs
@@ -11,10 +11,8 @@ pub use structs::Config;
 
 impl Config {
     pub fn new() -> Self {
-        let default_offensive_words = vec!["nigg", "sex", "porn", "igg"];
-        let default_tld_list = vec![
-            "mf", "btw", "fr", "yap", "dev", "scam", "zip", "root", "web", "rizz", "habibi", "sigma", "now", "it", "soy", "lol", "uwu", "ohio", "cat",
-        ];
+        let default_offensive_words = vec!["nigg", "sex", "porn", "igg", "fag", "hitler"]
+        let default_tld_list = vec!["com", "net", "org", "shit", "eefoc", "adrian", "67", "chat", "arsonflare", "gpt", "based", "delulu", "aura", "pmo", "sucks", "emo", "twin", "zorp", "clank", "web", "fent", "yeah", "slop", "job", "goat", "buss", "dawg", "opium", "gang", "ok", "wtf", "lol", "scam", "cat", "edge", "miku", "teto", "dumb", "balls", "yap", "attic", "ayo", "dev", "ac", "ad", "ae", "af", "ag", "ai", "al", "am", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "su", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"];
 
         Config {
             config_path: "config.toml".into(),
